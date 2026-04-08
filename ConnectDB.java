5a1b7ad5ab547b98267966c2fb02471afe888587
tthomsen22 <tom.thomsen9@gmail.com>
2026-04-08 11:48:21 -0500

    users and passwords

--- ConnectDB.java
+++ ConnectDB.java
@@ -6,9 +6,17 @@ public class ConnectDB {
 
     public static void main(String[] args) {
 
+        //String url = "jdbc:mysql://138.49.184.123:3306/neudahl2130_MovingLLC";
+        //String username = "skelton3801";
+        //String password = "qH3wcHuC7UBLwvdQ!";
+
         String url = "jdbc:mysql://138.49.184.123:3306/neudahl2130_MovingLLC";
-        String username = "skelton3801";
-        String password = "qH3wcHuC7UBLwvdQ!";
+        String username = "thomsen4642";
+        String password = "HJEksarADW7DtQq3-";
+
+        //String url = "jdbc:mysql://138.49.184.123:3306/neudahl2130_MovingLLC";
+        //String username = "neudahl2130";
+        //String password = "qH3wcHuC7UBLwvdQ!";
 
         try {
             Connection conn = DriverManager.getConnection(url, username, password);
